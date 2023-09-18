060d4c4b6d6b2e8ef76f586e00e33e2954a837cd
Christina Sørensen <christina@cafkafk.com>
2023-09-18 17:17:18 +0200

    refactor(search): remove unused module
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

--- src/main.rs
+++ src/main.rs
@@ -2,110 +2,6 @@ use clap::{arg, command, crate_authors, Arg};
 use std::env;
 use std::io;
 
-pub mod search {
-    //! A module for searching patterns within strings using regular expressions.
-    use std::error::Error;
-
-    use grep_matcher::Matcher;
-    use grep_regex::RegexMatcher;
-    use grep_searcher::sinks::UTF8;
-    use grep_searcher::Searcher;
-
-    // TODO: REMOVE THIS
-    const SHERLOCK: &'static [u8] = b"\
-    For the Doctor Watsons of this world, as opposed to the Sherlock
-    Holmeses, success in the province of detective work must always
-    be, to a very large extent, the result of luck. Sherlock Holmes
-    can extract a clew from a wisp of straw or a flake of cigar ash;
-    but Doctor Watson has to have it taken out for him and dusted,
-    and exhibited clearly, with a label attached.
-    ";
-
-    // TODO: REMOVE THIS
-    pub fn example() -> Result<String, Box<dyn Error>> {
-        let matcher = RegexMatcher::new(r"Doctor \w+")?;
-        let mut matches: Vec<(u64, String)> = vec![];
-        Searcher::new().search_slice(
-            &matcher,
-            SHERLOCK,
-            UTF8(|lnum, line| {
-                // We are guaranteed to find a match, so the unwrap is OK.
-                let mymatch = matcher.find(line.as_bytes())?.unwrap();
-                matches.push((lnum, line[mymatch].to_string()));
-                Ok(true)
-            }),
-        )?;
-
-        assert_eq!(matches.len(), 2);
-        assert_eq!(matches[0], (1, "Doctor Watsons".to_string()));
-        assert_eq!(matches[1], (5, "Doctor Watson".to_string()));
-        Ok(format!("{:#?}", matches))
-    }
-
-    /// Searches for a given pattern within an input string and returns the line
-    /// number and matched string.
-    ///
-    /// # Arguments
-    ///
-    /// * `input` - The input string to search within, represented as a byte slice.
-    /// * `pattern` - The pattern to search for, represented as a string slice.
-    ///
-    /// # Returns
-    ///
-    /// A `Result` containing a vector of tuples. Each tuple contains:
-    /// * A `u64` representing the line number where the match was found.
-    /// * A `String` representing the matched string.
-    ///
-    /// # Errors
-    ///
-    /// Returns an error if:
-    /// * The provided pattern is not a valid regular expression.
-    /// * There's an issue during the search operation.
-    pub fn search_string(
-        input: &[u8],
-        pattern: &str,
-    ) -> Result<Vec<(u64, String)>, Box<dyn Error>> {
-        let matcher = RegexMatcher::new(pattern)?;
-        let mut matches: Vec<(u64, String)> = vec![];
-        Searcher::new().search_slice(
-            &matcher,
-            input,
-            UTF8(|lnum, line| {
-                let mymatch = matcher.find(line.as_bytes())?.unwrap();
-                matches.push((lnum, line[mymatch].to_string()));
-                Ok(true)
-            }),
-        )?;
-
-        Ok(matches)
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-
-        /// Tests the `search_string` function with a simple pattern.
-        #[test]
-        fn search_string_simple() {
-            let res = search_string(
-                b"To the world you may be just one person, but to one person, you may be the world.",
-                r"person"
-            )
-            .unwrap();
-            assert_eq!(res.len(), 1);
-            assert_eq!(res[0], (1, "person".to_string()));
-        }
-
-        /// Tests the `search_string` function with a pattern that has no matches.
-        #[test]
-        fn search_string_no_match() {
-            let res = search_string(b"", r"not found").unwrap();
-            assert_eq!(res.len(), 0);
-            assert_eq!(res, vec![]);
-        }
-    }
-}
-
 fn main() -> io::Result<()> {
     let matches = command!()
         .author(crate_authors!("\n"))
@@ -211,7 +107,6 @@ pub mod fortune {
     //! A module for retrieving random quotes (or fortune).
     use crate::file;
     use crate::random;
-    use crate::search;
 
     use std::process::exit;
 
